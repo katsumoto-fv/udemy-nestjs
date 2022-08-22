acf436c01c3a504e0fa97e33a8928c8f7b0ebbfb
勝本宏樹 <katsumoto@fvs-net.co.jp>
2022-08-22 15:01:34 +0900

    コミット

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ItemsModule } from './items/items.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [ItemsModule, TypeOrmModule.forRoot(), AuthModule],
   controllers: [],
   providers: [],
 })
